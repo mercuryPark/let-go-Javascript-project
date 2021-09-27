4d43cf97db200190d2e8b1d5de9ab496a517ce38
mercuryPark <76655229+mercuryPark@users.noreply.github.com>
2021-09-27 17:23:17 +0900

    # 1

---
+++ main.js
@@ -0,0 +1,177 @@
+// var iceLand = 'bbq';
+
+// iceLand = '1123dkk';
+
+// const iceLand2 = '사과나무안에 있는 번데기';
+
+// iceLand2 = '오렌지나무 안에 있는 작은 곤충';
+
+// console.log(iceLand);
+
+// console.log(iceLand2);
+
+// let b = 3;
+// let a = 5;
+
+//  let temp = a;
+// a = b;
+// b = a;
+
+// let condition1 = true;
+// let condition2 = false;
+// let value = condition1 ? (condition2 ? '둘 다 참' : 'condition만 참') : 'condition1이 거짓';
+
+// console.log(value);
+
+// let cond = true;
+// let value = '';
+
+// if (cond) {
+//     value = '참';
+// } else {
+//     value = '거짓';
+// }
+
+
+
+// switch(cond) {
+//     case true:
+//         value = ('참');
+//         break;
+        
+//      case false: 
+//         value = ('거짓');
+// }
+// console.log(value);
+
+// let cond = true;
+// let value = cond ? '참' : '거짓';
+
+// console.log(cond);
+
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     for(let j = 0; j < 10; j++) {
+//         console.log(i, j);
+//     }
+// }
+
+
+// for (let i = 0; i < 5; i++){
+//     if(i % 2 === 0) continue;
+//     for (let j = 0; j < 5; j++) {
+//         if(j % 2 === 0) continue;
+//         for(let k = 0; k < 5; k++){
+//             if(k % 2 === 0) continue;
+//             console.log(i, j, k);
+//         }
+//     }
+// }
+
+// for (let i = 1; i < 10; i++){
+//     if(i % 2 === 0) continue;
+//     for (let j = 1; j < 10; j++){
+//         if(j % 2 === 0) continue;
+//         console.log(i, '*', j,'=', i*j);
+//     }
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//         if(i % 2 === 0 || j % 2 === 0) continue;
+//         console.log(i, '*', j,'=', i*j);
+//     }
+// }
+
+
+// const arr = ['가', '나', '다', '라', '바'];
+// arr[arr.length] = '마'
+// console.log(arr);
+
+
+// const arr = ['가', '라', '다', '라', '마', '라'];
+
+// let index = arr.indexOf('라');
+
+// while (index > -1) {
+
+//     arr.splice(index, 1);
+
+//     index = arr.indexOf('라');
+// }
+
+
+// function a(w, x, y, z){
+//     console.log(w, x, y, z);
+//     console.log(arguments);
+// }
+
+// a('Hello', 'Parameter', 'Argument');
+
+// function add(x, y) {
+//    return x + y;
+// }
+
+// console.log(add(3, 5));
+
+// const c =(x, y, z) => {
+// return x * y * z;
+// }
+
+
+// console.log(c(3, 5, 6));
+
+// const zerocho = {
+//     name: {
+//         first: '현영',
+//         last: '조',
+//     },
+//     gender: 'm',
+
+    
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
